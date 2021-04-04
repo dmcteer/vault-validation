945eb97a7483be911269ab5eed5aa51df6601e31
Dan McTeer <dmcteer@gmail.com>
2021-04-04 14:44:42 -0600

    initial push

---
+++ main.tf
@@ -0,0 +1,60 @@
+provider "digitalocean" {
+  token = var.DO_PAT
+}
+
+resource "digitalocean_droplet" "vault" {
+  count = "1"
+  image = var.IMAGE
+  name = element(var.HOST_NAMES, count.index)
+  region = var.REGION
+  size = var.INST_TYPE
+  private_networking = true
+  ssh_keys = [
+    var.SSH_FP
+  ]
+
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.PVT_KEY)
+    timeout = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "yum install -y https://repo.saltstack.com/py3/redhat/salt-py3-repo-latest.el7.noarch.rpm",
+      "yum install -y salt-minion",
+      "mkdir -p /srv/salt",
+      "systemctl enable salt-minion.service",
+      "systemctl restart salt-minion"
+    ]
+  }
+
+  provisioner "file" {
+    source = "saltstack/pillar"
+    destination = "/srv"
+  }
+
+  provisioner "file" {
+    source = "saltstack/salt/vault"
+    destination = "/srv/salt"
+  }
+
+  provisioner "file" {
+    source = "saltstack/salt/files"
+    destination = "/srv/salt"
+  }
+
+  provisioner "file" {
+    source = "saltstack/salt/top.sls"
+    destination = "/srv/salt/top.sls"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "salt-call --local state.apply vault",
+      "systemctl start vault"
+    ]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "DO_PAT" {
+  description = "DigitalOcean API Key"
+}
+
+variable "PVT_KEY" {
+  description = "The full path to the SSH private key for DO Instances"
+  default = "/Users/mcteer/.ssh/id_rsa_do"
+}
+
+variable "SSH_FP" {
+  default = "18:7b:12:c5:0e:f9:94:91:a4:f7:0f:c3:5e:55:3a:e6"
+}
+
+variable "IMAGE" {
+  default = "centos-7-x64"
+}
+
+variable "INST_TYPE" {
+  default = "1gb"
+}
+
+variable "REGION" {
+  default = "sfo2"
+}
+
+variable "HOST_NAMES" {
+  type = list(string)
+  default = ["vault1", "vault2", "vault3"]
+}
